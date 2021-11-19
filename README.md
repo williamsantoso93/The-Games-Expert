59a8b1f6ad9c5d3b26668909747408e57d944f07
William Santoso <wsantoso@otospector.co.id>
2021-11-20 01:14:12 +0700

    cleanup core data

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ Assumption :
 * Create in Xcode 13.1 and use iOS 15.0 as deployment
 * Tested using iPhone 11 simulator and iPhone Xr real device
 * User only use iPhone
-* UI created by using SwiftUI (SwiftLint works differently when using SwiftUI)
+* UI created by using SwiftUI
 * SwiftLint error mostly in pods
 * Use userDefaults for name & social media
-* Use coreData for favorite
+* Use realm for favorite
 * Use Combine as Reactive Programming
-* Use manual injection ans Dependency Injection (implement in DetailScreen & DetailViewModel)
+* Use manual injection as Dependency Injection
 
 
 Used third party library (cocoapods) :
@@ -22,3 +22,4 @@ Used third party library (cocoapods) :
 
 Swift Package Manager : 
 * SwiftUIX
+* Realm
--- Shared/App/TheGamesApp.swift
+++ Shared/App/TheGamesApp.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 @main
 struct TheGamesApp: App {
-    let persistenceController = PersistenceController.shared
     @Environment(\.scenePhase) var scenePhase
     
     let homeViewModel = HomeViewModel(homeUseCase: Injection.init().provideHome())
@@ -17,20 +16,7 @@ struct TheGamesApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView(viewModel: homeViewModel)
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
                 .environmentObject(homeViewModel)
-        }
-        .onChange(of: scenePhase) { newScenePhase in
-            switch newScenePhase {
-            case .background:
-                persistenceController.save()
-            case .inactive:
-                break
-            case .active:
-                break
-            @unknown default:
-                break
-            }
         }
     }
 }
